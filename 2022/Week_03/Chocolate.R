7ddc99ba50adb995ab6159ab980f75cf916cc4ac
Stephen Brennan <stiofanob@gmail.com>
2022-01-24 09:09:49 +0000

    Added wordcloud

--- 2022/Week_03/Chocolate.R
+++ 2022/Week_03/Chocolate.R
@@ -2,11 +2,13 @@
 
 # Load the required packages
 library(tidyverse)
+library(tidytuesdayR)
+library(wordcloud)
 
 # Read in the latest data
-RawData <- tidytuesdayR::last_tuesday() %>%
-  tidytuesdayR::tt_load_gh() %>%
-  tidytuesdayR::tt_download()
+RawData <- as.Date("2022-01-18") %>%
+  tt_load_gh() %>%
+  tt_download()
 
 # Extract the Chocolate data
 ChocData <- RawData$chocolate
@@ -24,3 +26,19 @@ ChocData %>%
   ggplot(aes(x = rating, y = country_of_bean_origin)) +
   ggridges::geom_density_ridges() +
   scale_x_continuous(limits = c(2,5))
+
+
+df <- str_split_fixed(ChocData$most_memorable_characteristics, ",", 10) %>%
+  as_tibble() %>%
+  mutate(OriginCountry = ChocData$company_location) %>%
+  pivot_longer(V1:V10) %>%
+  mutate(value = trimws(value)) %>%
+  filter(value != "") %>%
+  group_by(value) %>%
+  summarize(Count = n())
+
+
+set.seed(1234) # for reproducibility
+
+wordcloud(words = df$value, freq = df$Count, min.freq = 1,
+          max.words=200, random.order=FALSE, rot.per=0.35)
